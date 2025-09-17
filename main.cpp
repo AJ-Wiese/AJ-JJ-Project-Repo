89354515bbb92fd16d1448a2715a1df1a0b4ca70
AJ-Wiese <awiese0409@gmail.com>
2025-09-17 10:57:02 -0500

    Have working Color Generator!! doesn't make all colors yet

--- main.cpp
+++ main.cpp
@@ -6,17 +6,16 @@ using namespace std;
 
 int main() {
     srand(time(0)); //Makes random random
+
     //making variable for colors//
-    int r1Variable = rand()%16;
-    if(r1Variable==16) {
+    int r1Variable = rand()%9;
+    int r2Variable = rand()%9;
+    int g1Variable = rand()%9;
+    int g2Variable = rand()%9;
+    int b1Variable = rand()%9;
+    int b2Variable = rand()%9;
 
-    }
-
-    int gVariable = rand()%16;
-
-    int bVariable = rand()%16;
-
-    cout << r1Variable << endl;
-    cout << gVariable << endl;
-    cout << bVariable << endl;
+    //Output color//
+    cout <<"Random Color:"<< r1Variable<< r2Variable<<g1Variable
+    <<g2Variable<<b1Variable<<b2Variable<< endl;
 }
